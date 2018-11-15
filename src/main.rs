13f5e3d78a7bd70f1324ca9b9990e9f8bc3498f6
Arun Sivaramakrishnan <arun.sivaramakrishnan@altair.com>
2018-11-16 00:04:23 +0530

    rule parsing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ named!(till_endbrace, take_until!(")\n"));
 // rvalue is something that can appear on the right side of assignment statement
 #[derive(Debug)]
 enum RvalGeneral {
-    Prefix(String), //  a normal string
+    Prefix(String), // a normal string
     DollarExpr(String), // this is dollar expr eg $(EXPR)
     AtExpr(String), // @(EXPR)
+    Group(Vec<RvalGeneral>),
+    Bucket(String),
 }
 
 #[derive(Debug)]
@@ -26,62 +28,47 @@ struct Ident {
 fn to_lval(s: &str) -> Ident {
     Ident { name: s.to_owned() }
 }
-// #[derive(Debug)]
-// struct Assignment {
-//     left: Ident,
-//     right: Vec<RvalGeneral>,
-//     is_append: bool,
-// }
+
 #[derive(Debug)]
-struct Bucket
-{
-    name: String
-}
-#[derive(Debug)]
-struct Group
-{
-   name : String
+struct Source {
+    primary: Vec<RvalGeneral>,
+    foreach: bool,
+    secondary: Vec<RvalGeneral>,
 }
 
 #[derive(Debug)]
-struct IOValGeneral
-{
-    paths: Vec<RvalGeneral>,
-    buckets: Vec<Bucket>,
-    groups : Vec<Group>
+struct Target {
+    primary: Vec<RvalGeneral>,
+    secondary: Vec<RvalGeneral>,
+    tag: Vec<RvalGeneral>,
 }
 
 #[derive(Debug)]
-enum Source
-{
-    Primary{ inp: IOValGeneral, foreach: bool},
-    Secondary(IOValGeneral)
+struct RuleFormula {
+    description: String,
+    formula: Vec<RvalGeneral>,
 }
 
-#[derive(Debug)]
-enum Target
-{
-    Primary(RvalGeneral),
-    Secondary(RvalGeneral),
-    BucketName(String),
-    GroupName(String)
-}
 
 #[derive(Debug)]
-struct Rule
-{
-    disp_string: String,
-    rule_string: String,
-}
-
-#[derive(Debug)]
-enum Statement
-{
-    LetExpr{left:Ident, right:Vec<RvalGeneral>, is_append: bool},
-    IfElseEndIf{ eq: EqCond, then_statements: Box<Vec<Statement>> , else_staments: Box< Vec<Statement>> },
-    IncRules(Box<Vec<Statement>>),
-    Include(Box<Vec<Statement>>),
-    Link{s: Source, t:Target, r:Rule}
+enum Statement {
+    LetExpr {
+        left: Ident,
+        right: Vec<RvalGeneral>,
+        is_append: bool,
+    },
+    IfElseEndIf {
+        eq: EqCond,
+        then_statements: Box<Vec<Statement>>,
+        else_staments: Box<Vec<Statement>>,
+    },
+    IncludeRules,
+    Include(String),
+    Link {
+        s: Source,
+        t: Target,
+        r: RuleFormula,
+    },
 }
 
 // convert byte str to RvalGeneral::Prefix
@@ -97,15 +84,18 @@ fn is_ident(c: u8) -> bool {
 }
 // parse rvalue wrapped inside dollar or at
 named!(parse_rvalue_raw,
-       delimited!(alt!(tag!("$(") | tag!("@(") ), take_while!(is_ident), tag!(")"))
-);
+       delimited!(alt!(tag!("$(") | tag!("@(")),
+                  take_while!(is_ident),
+                  tag!(")")));
 
 named!(parse_rvalue_raw_bucket,
-       delimited!(tag!("{"), take_while!(is_ident), tag!("}"))
-);
+       delimited!(tag!("{"), take_while!(is_ident), tag!("}")));
 
-// named!(parse_rvalue_raw_group,
-//        delimited!(tag!("<"), alt!(map_res!(take_while!(is_ident),  ) | many!(parse_rvalue_raw) ), tag!(">")));
+named!(parse_rvalue_raw_angle<&[u8], Vec<RvalGeneral>>,
+       do_parse!(tag!("<") >>
+                 v: apply!(parse_rvalgeneral_list_long, ">") >>
+                 tag!(">") >> (v.0))
+);
 
 // parse rvalue at expression eg @(V)
 named!(parse_rvalue_at<&[u8], RvalGeneral>,
@@ -114,6 +104,7 @@ named!(parse_rvalue_at<&[u8], RvalGeneral>,
        (RvalGeneral::AtExpr(rv.to_owned()))
    )
 );
+
 // parse rvalue dollar expression eg $(H)
 named!(parse_rvalue_dollar<&[u8], RvalGeneral>,
    do_parse!(
@@ -121,12 +112,30 @@ named!(parse_rvalue_dollar<&[u8], RvalGeneral>,
        (RvalGeneral::DollarExpr(rv.to_owned()))
    )
 );
+named!(parse_rvalue_angle<&[u8], RvalGeneral>,
+       do_parse!(
+           rv : parse_rvalue_raw_angle >>
+               (RvalGeneral::Group(rv))
+       )
+);
+named!(parse_rvalue_bucket<&[u8], RvalGeneral>,
+       do_parse!(
+           rv : map_res!(parse_rvalue_raw_bucket, std::str::from_utf8) >>
+               (RvalGeneral::Bucket(rv.to_owned()))
+       )
+);
+
+
+
 named!(parse_rvalue<&[u8], RvalGeneral>,
        switch!(peek!(take!(1)),
            b"$" => call!(parse_rvalue_dollar) |
-           b"@" => call!(parse_rvalue_at)
+               b"@" => call!(parse_rvalue_at) |
+               b"<" => call!(parse_rvalue_angle) |
+               b"{" => call!(parse_rvalue_bucket)
        )
 );
+
 // eat up the dollar or at that dont parse to dollar expression or at expression
 named!(chompdollar,
        do_parse!( peek!(one_of!("$@")) >> not!(parse_rvalue_raw) >> r : take!(1)  >> (r)));
@@ -135,7 +144,7 @@ named!(chompdollar,
 fn parse_greedy<'a, 'b>(input: &'a [u8],
                         delim: &'b str)
                         -> Result<(&'a [u8], &'a [u8]), nom::Err<&'a [u8]>> {
-    let mut s = String::from("\\\n$@");
+    let mut s = String::from("\\\n$@{<");
     s.push_str(delim);
     alt!(input,
          value!(b"".as_ref(), complete!(tag!("\\\n"))) | chompdollar |
@@ -155,22 +164,30 @@ fn parse_rvalgeneral_list_long<'a, 'b>
      -> Result<(&'a [u8], (Vec<RvalGeneral>, &'a [u8])), nom::Err<&'a [u8]>> {
     many_till!(input,
                apply!(parse_rvalgeneral, delim),
-               alt!(tag!(delim) | eof!())  )
+               alt!(tag!(delim) | eof!()))
 }
 // specialization of previos one delimited by eol
 named!(parse_rvalgeneral_list<&[u8], (Vec<RvalGeneral>, &[u8]) >,
        apply!(parse_rvalgeneral_list_long, "\n") );
+
 // parse a lvalue to a string
 named!(parse_lvalue<&[u8], Ident>,
         do_parse!(  l : map_res!(take_while!(is_ident), std::str::from_utf8)>> (to_lval(l)) )
-       );
+);
 
-// named!(parse_include<&[u8], Statement>,
-//        do_parse!(ws!(tag!("include")) >> s: map_res!(take_until!("\n"), std::str::from_utf8) >> (Statement::Include(String::from(s)))
-//        )
-// );
+named!(parse_include<&[u8], Statement>,
+       do_parse!(ws!(tag!("include")) >> s: map_res!(take_until!("\n"), std::str::from_utf8) >>
+                 (Statement::Include(String::from(s)))
+        )
+);
 
-       // parse an assignment expr
+named!(parse_include_rules<&[u8], Statement>,
+       do_parse!(ws!(tag!("include_rules")) >> _s: map_res!(take_until!("\n"), std::str::from_utf8) >>
+                 (Statement::IncludeRules)
+       )
+);
+
+// parse an assignment expr
 named!(parse_let_expr<&[u8], Statement>,
        do_parse!( l : ws!(parse_lvalue) >>
                   op : ws!(alt_complete!( tag!("=") | tag!("+=")  )) >>
@@ -178,15 +195,65 @@ named!(parse_let_expr<&[u8], Statement>,
                   (Statement::LetExpr{ left:l, right:r.0,
                                is_append: (op == b"+=") }) )
 );
-//       named!(parse_include<&[u8], Vec<Statement>>,
+named!(parse_rule_gut<&[u8], RuleFormula>,
+       do_parse!(
+           description :
+           ws!(opt!(do_parse!( tag!("^") >>
+                               r: map_res!(take_until!("^"), std::str::from_utf8) >>
+                               tag!("^") >> (String::from(r)) ) ) ) >>
+               formula : apply!(parse_rvalgeneral_list_long, "|") >>
+               (RuleFormula { description : description.unwrap_or(String::from("")), formula : formula.0}) )
+);
 
+fn from_input(primary: Vec<RvalGeneral>, foreach: bool, secondary: Vec<RvalGeneral>) -> Source {
+    Source {
+        primary: primary,
+        foreach: foreach,
+        secondary: secondary,
+    }
+}
 
-// named!(parse_statement<&[u8], Statement>,
-//        alt_complete!(
-//            IncRules,
-//            Include(String)
-//        )
-// );
+fn from_output(primary: Vec<RvalGeneral>,
+               secondary: Vec<RvalGeneral>,
+               tag: Vec<RvalGeneral>)
+               -> Target {
+    Target {
+        primary: primary,
+        secondary: secondary,
+        tag: tag,
+    }
+}
+named!(parse_rule<&[u8], Statement>,
+      do_parse!( tag!(":") >>
+                 for_each: opt!(ws!(tag!("foreach"))) >>
+                 input : apply!(parse_rvalgeneral_list_long, "|") >>
+                 secondary_input : opt!( do_parse!(tag!("|") >>  r : apply!(parse_rvalgeneral_list_long, "|") >> (r)) ) >>
+                 tag!(">") >>
+                 rule_formula : parse_rule_gut >>
+                 tag!(">") >>
+                 secondary_output : opt!( do_parse!( r: apply!(parse_rvalgeneral_list_long, "|") >> tag!("|") >> (r)) ) >>
+                 output : apply!(parse_rvalgeneral_list_long, "\n") >>
+              //   opt!(eat_separator!(" \t")) >>
+                 outputtag : opt!(parse_rvalgeneral_list) >>
+                 (Statement::Link {
+                     s : from_input(input.0, for_each.is_some(),
+                                    secondary_input.unwrap_or((Vec::new(), b"")).0),
+                     t : from_output(output.0, secondary_output.unwrap_or((Vec::new(), b"")).0, outputtag.unwrap_or((Vec::new(), b"")).0),
+                     r : rule_formula
+                     }
+                 ))
+);
+
+named!(parse_statement<&[u8], Statement>,
+       alt_complete!( parse_include |
+                      parse_include_rules|
+                      parse_let_expr |
+                      parse_rule)
+);
+
+named!(parse_statements_until<&[u8], (Vec<Statement>, &[u8])>,
+       many_till!(parse_statement, alt_complete!(tag!("endif")| tag!("else")))
+);
 
 // parse equality condition (only the condition, not the statements that follow if)
 named!(parse_eq<&[u8], EqCond>,
@@ -202,7 +269,7 @@ fn main() {
     let res1 = parse_eq(b" ifeq($(HW_DEBUG),20)\nvar x=y");
     println!("res1: {:?}", res1);
     let res2 = parse_rvalue(b"$(HW_DEBUG)x");
-    println!("{:?}\n",  res2);
+    println!("{:?}\n", res2);
     let res3 = parse_rvalgeneral(b"help help$(HW_DEBUG)\n", "");
     let inp = b"geko $(help)\\\ngeko geko\\\ngg@(H),$(V) \n";
     let res4 = do_parse!(&inp,
@@ -213,7 +280,7 @@ fn main() {
     let res4 = std::str::from_utf8(parse_greedy(b"a,$(h)_", ",").unwrap().1);
 
     println!("r4:{:?}", res4);
-    let inp = b" SOURCES_  += sdsd$(HW_ROOTDIR).cxx \n".as_ref(); 
+    let inp = b" SOURCES_  += sdsd$(HW_ROOTDIR).cxx \n".as_ref();
     let res41 = do_parse!(inp,
                           map_res!(ws!(take_while!(is_ident)), std::str::from_utf8)>>
                           alt_complete!(tag!("=") | tag!("+=")) >>
@@ -222,4 +289,11 @@ fn main() {
     let res5 = parse_let_expr(b"SOURCES += $(HW_ROOTDIR)/*.cxx  \n");
     println!("{:?}", res5);
 
+    let res6 = parse_rule(b": {objs} |> p4 edit %i |> %B.o \n");
+    match res6 {
+        Ok(something) => println!("{:?}", something),
+        Err(nom::Err::Error(nom::Context::Code(x,_y))) => println!("{:?}", std::str::from_utf8(x).to_owned() ),
+        _ => println!("unknown")
+    }
+    //println!("{:?}", res6);
 }
